4b3855563f6313af77f9b87ceeb6b24873de0080
miwk26 <yshdjwue@gmail.com>
2026-05-07 16:00:00 +0900

    [Feat] 과제

--- c_shyun mid term/Assets/Script/PlayerController.cs
+++ c_shyun mid term/Assets/Script/PlayerController.cs
@@ -53,6 +53,9 @@ public class PlayerController : MonoBehaviour
 
         if (collision.CompareTag("Finish"))
         {
+            //HighScore.TrySet(SceneManager.GetActiveScene().buildIndex, (int)score);
+            StageResultSaver.SaveStage(SceneManager.GetActiveScene().buildIndex, (int)score);
+
             collision.GetComponent<LevelObject>().MoveToNextLevel();
         }
 
